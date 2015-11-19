00db7b7f03bd8154d26bf4aa08d6a6f2af8d606c
Munsterberg <jetrussell93@gmail.com>
2015-11-19 18:47:12 -0500

    Initial commit

---
+++ index.php
@@ -0,0 +1,5 @@
+<?php include_once 'partials/head.php'; ?>
+<?php include_once 'partials/nav.php'; ?>
+
+
+<?php include_once 'partials/footer.php'; ?>
---
+++ partials/head.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html class="no-js" lang="">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <title></title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <link rel="apple-touch-icon" href="apple-touch-icon.png">
+        <!-- Place favicon.ico in the root directory -->
+
+        <link href='https://fonts.googleapis.com/css?family=Arimo|Noto+Sans:400,700' rel='stylesheet' type='text/css'>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+        <link rel="stylesheet" href="css/main.css">
+        <link rel="stylesheet" href="css/normalize.css">
+        <script src="js/vendor/modernizr-2.8.3.min.js"></script>
+    </head>
