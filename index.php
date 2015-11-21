e2df990c031f11214c8201c728ce840b54816ada
Munsterberg <jetrussell93@gmail.com>
2015-11-21 18:15:02 -0500

    Mobile navigation created.

--- index.php
+++ index.php
@@ -3,13 +3,31 @@
 
 <div class="wrapper">
   <header class="main-header">
-    <div class="nav-trigger"><i class="fa fa-bars"></i></div>
+    <div class="menu-collapsed">
+      <div class="nav-trigger"><i class="fa fa-bars"></i></div>
+      <nav>
+        <ul>
+          <li><a href="#">Home</a></li>
+          <li><a href="#">Portfolio</a></li>
+          <li><a href="#">About</a></li>
+        </ul>
+      </nav>
+    </div>
     <div class="logo"><h1>Logo</h1></div>
   </header>
   <div class="content">
     <div class="container">
       <h1><span>Hi</span>, I am <span>Jacob Russell</span>. A professional <span>Software Engineer</span> with a focus
       on <span>Website Development and Design</span>.</h1>
+      <h1>Hellool aodasodmaomd asomdasmdmoadmam  oamsdma mdmao dasodm asodmam mdasmomd asmdoam
+       saomdoamodmaosmd mamdmasod accesskey="as domaoda mdosamdo amodmoamdmoams oda "daom oa
+      asomdoam dmaosm dmamod m</h1>
+      <h1>Hellool aodasodmaomd asomdasmdmoadmam  oamsdma mdmao dasodm asodmam mdasmomd asmdoam
+       saomdoamodmaosmd mamdmasod accesskey="as domaoda mdosamdo amodmoamdmoams oda "daom oa
+      asomdoam dmaosm dmamod m</h1>
+      <h1>Hellool aodasodmaomd asomdasmdmoadmam  oamsdma mdmao dasodm asodmam mdasmomd asmdoam
+       saomdoamodmaosmd mamdmasod accesskey="as domaoda mdosamdo amodmoamdmoams oda "daom oa
+      asomdoam dmaosm dmamod m</h1>
     </div>
   </div>
 </div>
---
+++ js/main.js
@@ -0,0 +1,4 @@
+$(".menu-collapsed").click(function() {
+  $(this).toggleClass("menu-expanded");
+  $(".nav-trigger").toggleClass("nav-hidden");
+});
