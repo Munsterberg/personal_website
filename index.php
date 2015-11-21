9ae9d24811b2b64b39117424d43de8b1f651a121
Munsterberg <jetrussell93@gmail.com>
2015-11-21 01:41:34 -0500

    Header and mockups added.

--- index.php
+++ index.php
@@ -1,5 +1,11 @@
 <?php include_once 'partials/head.php'; ?>
 <?php include_once 'partials/nav.php'; ?>
 
+<div class="wrapper">
+  <header class="main-header">
+    <div class="nav-trigger"><i class="fa fa-bars"></i></div>
+    <div class="logo"><h1>Logo</h1></div>
+  </header>
+</div>
 
 <?php include_once 'partials/footer.php'; ?>
