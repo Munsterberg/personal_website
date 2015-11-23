bde0ff77d6ac931c6801a7aed0ef645f091dad62
Munsterberg <jetrussell93@gmail.com>
2015-11-23 03:17:44 -0500

    New colour scheme added.

--- index.php
+++ index.php
@@ -1,25 +1,18 @@
 <?php include_once 'partials/head.php'; ?>
-<?php include_once 'partials/nav.php'; ?>
 
 <div class="wrapper">
-  <header class="main-header">
-    <div class="menu-collapsed">
-      <div class="nav-trigger"><i class="fa fa-bars"></i></div>
-      <nav>
-        <ul>
-          <li><a href="#">Home</a></li>
-          <li><a href="#">Work</a></li>
-          <li><a href="#">About</a></li>
-        </ul>
-      </nav>
-    </div>
-    <div class="logo"><h1>Logo</h1></div>
-  </header>
+<?php include_once 'partials/nav.php'; ?>
   <div class="content">
     <div class="title-area">
       <div class="container">
-        <h1><span>Hi</span>, I am <span>Jacob Russell</span>. A professional <span>Software Engineer</span> with a focus
-        on <span>Website Development and Design</span>.</h1>
+        <h1><span>Hello</span>. Looking for professional <span>Web Design and
+          Development</span>?</h1>
+        <p>My name is <span>Jacob Russell</span>, and I'm a <span>Software Engineer
+        </span> with a focus on <span>Web Design and Development</span>. Using
+        tools like <span>Bootstrap, SASS, JavaScript, jQuery, Angular,
+        PHP/Laravel, Python/Django, and Node.js</span>, you can rely on me to create a
+        beautiful
+        <span>responsive website</span>, or a full-scale <span>web application</span>.</p>
       </div>
     </div>
     <div class="display-area">
---
+++ partials/nav.php
@@ -0,0 +1,13 @@
+<header class="main-header">
+  <div class="menu-collapsed">
+    <div class="nav-trigger"><i class="fa fa-bars"></i></div>
+    <nav>
+      <ul>
+        <li><a href="#">Home</a></li>
+        <li><a href="#">Work</a></li>
+        <li><a href="#">About</a></li>
+      </ul>
+    </nav>
+  </div>
+  <div class="logo"><h1>Logo</h1></div>
+</header>
