cf8619d832d3c6f5adb55b740e2ad6bce07c66bc
Munsterberg <jetrussell93@gmail.com>
2015-11-26 01:33:08 -0500

    New colour scheme, logo fixes, and display area hover effects

--- index.php
+++ index.php
@@ -5,22 +5,22 @@
   <div class="content">
     <div class="title-area">
       <div class="container">
-        <h1>Professional <span>Design and Development</span> for full-scale web
+        <h1>Professional <span>Web Design and Development</span> for full-scale web
         applications.</h1>
         <p>My name is <span>Jacob Russell</span>, and I'm a <span>Software Engineer
         </span> with a focus on <span>Web Design and Development</span>. Using
-        tools like <span>Bootstrap, SASS, JavaScript, jQuery, Angular,
+        tools like <span>Bootstrap, SASS, JavaScript, jQuery, Angular, Java,
         PHP/Laravel, Python/Django, and Node.js</span>, you can rely on me to create a
         beautiful
         <span>responsive website</span>, or a database driven <span>web application</span>.</p>
       </div>
     </div>
     <div class="display-area">
-      <div class="fluid-container">
-        <a href="#"><img class="col-md-4" src="/img/munsterberg.jpg" alt=""></a>
-        <a href="#"><img class="col-md-4" src="/img/nerdorade.jpg" alt=""></a>
-        <a href="#"><img class="col-md-4" src="/img/turnkeyss.jpg" alt=""></a>
-        <a href="#"><img class="col-md-4" src="/img/nodeblog.jpg" alt=""></a>
+      <div class="container-fluid">
+        <a href="#"><img class="col-md-4 img-responsive" src="/img/munsterberg.jpg" alt=""></a>
+        <a href="#"><img class="col-md-4 img-responsive" src="/img/nerdorade.jpg" alt=""></a>
+        <a href="#"><img class="col-md-4 img-responsive" src="/img/turnkeyss.jpg" alt=""></a>
+        <a href="#"><img class="col-md-4 img-responsive" src="/img/nodeblog.jpg" alt=""></a>
       </div>
     </div>
   </div>
