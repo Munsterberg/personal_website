f2220c62b5ec0917774e14b93ef479538dd0b445
Munsterberg <jetrussell93@gmail.com>
2015-11-28 15:41:20 -0500

    Contact page structured.

---
+++ contact.php
@@ -0,0 +1,15 @@
+<?php include_once 'partials/head.php'; ?>
+
+<div class="wrapper">
+  <?php include_once 'partials/nav.php'; ?>
+  <div class="content">
+    <div class="container">
+      <div class="profile-area">
+        <img src="/img/me.jpg" class="profile-img" alt="A picture of Jacob Russell">
+        <h1>Meet, Jacob Russell!</h1>
+      </div>
+    </div>
+  </div>
+</div>
+
+<?php include_once 'partials/footer.php'; ?>
--- index.php
+++ index.php
@@ -10,9 +10,9 @@
         <p>My name is <span>Jacob Russell</span>, and I'm a <span>Software Engineer
         </span> with a focus on <span>Web Design and Development</span>. Using
         tools like <span>Bootstrap, SASS, JavaScript, jQuery, Angular, Java,
-        PHP/Laravel, Python/Django, and Node.js</span>, you can rely on me to create a
+        PHP/Laravel, Python/Django, and Node.js,</span> you can rely on me to create a
         beautiful
-        <span>responsive website</span>, or a database driven <span>web application</span>.</p>
+        <span>responsive website,</span> or a database driven <span>web application</span>.</p>
       </div>
     </div>
     <div class="display-area">
